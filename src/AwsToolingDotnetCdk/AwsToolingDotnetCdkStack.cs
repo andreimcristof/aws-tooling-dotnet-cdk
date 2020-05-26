81689b275cfe3f2c8586c505bda1236d245fd14c
andreimcristof <andreimcristof@gmail.com>
2020-05-26 07:49:21 +0200

    _

---
+++ src/AwsToolingDotnetCdk/AwsToolingDotnetCdkStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace AwsToolingDotnetCdk
+{
+    public class AwsToolingDotnetCdkStack : Stack
+    {
+        internal AwsToolingDotnetCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ src/AwsToolingDotnetCdk/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AwsToolingDotnetCdk
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new AwsToolingDotnetCdkStack(app, "AwsToolingDotnetCdkStack");
+            app.Synth();
+        }
+    }
+}
